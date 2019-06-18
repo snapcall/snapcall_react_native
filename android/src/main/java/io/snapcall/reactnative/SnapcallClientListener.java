dec6b89a1040d5d33d925a4f7ccc2b168c4ed0e4
pnoyelle <noyelle.pierre@gmail.com>
2019-06-18 19:18:14 +0200

    android added : class to handle the scclient event

---
+++ android/src/main/java/io/snapcall/reactnative/SnapcallClientListener.java
@@ -0,0 +1,214 @@
+package io.snapcall.reactnative;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import javax.annotation.Nullable;
+
+import io.snapcall.snapcall_android_framework.SCCall;
+import io.snapcall.snapcall_android_framework.SCClient;
+import io.snapcall.snapcall_android_framework.SCClientEvent;
+import io.snapcall.snapcall_android_framework.SCClientListener;
+
+class SnapcallClientListener implements SCClientListener {
+
+    private static final String TAG = "SnapcallClientListener";
+    private ReactContext context;
+
+    SnapcallClientListener(ReactContext context) {
+
+        this.context = context;
+    }
+
+    void release() {
+
+        context = null;
+    }
+
+    private void sendEvent(String eventName, @Nullable SCClientEvent event) {
+        try {
+            Log.d(TAG, String.format("event Name : %s ", eventName));
+            final WritableMap params = Arguments.createMap();
+            params.putString("event", eventName);
+            if (event != null) {
+                params.putString("data", convertSCClientEventToJSONString(event));
+            }
+            if (context != null) {
+                context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit(eventName, params);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionReady(SCClientEvent parameter) {
+
+        sendEvent("onConnectionReady", parameter);
+    }
+
+    @Override
+    public void onCreated(SCClientEvent parameter) {
+
+        sendEvent("onCreated", parameter);
+    }
+
+    @Override
+    public void onRinging(SCClientEvent parameter) {
+
+        sendEvent("onRinging", parameter);
+    }
+
+    @Override
+    public void onAnswer(SCClientEvent parameter) {
+
+        sendEvent("onAnswer", parameter);
+    }
+
+    @Override
+    public void onInternetDown(SCClientEvent parameter) {
+
+        sendEvent("onInternetDown", parameter);
+    }
+
+    @Override
+    public void onInternetUP(SCClientEvent parameter) {
+
+        sendEvent("onInternetUP", parameter);
+    }
+
+    @Override
+    public void onHangup(SCClientEvent parameter) {
+
+        sendEvent("onHangup", parameter);
+    }
+
+    @Override
+    public void onHeld(SCClientEvent parameter) {
+
+        sendEvent("onHeld", parameter);
+    }
+
+    @Override
+    public void onUnheld(SCClientEvent parameter) {
+
+        sendEvent("onUnheld", parameter);
+    }
+
+    @Override
+    public void onTime(SCClientEvent parameter) {
+
+        sendEvent("onTime", parameter);
+    }
+
+    @Override
+    public void onConnectionShutDown() {
+
+        sendEvent("onConnectionShutDown", null);
+    }
+
+    @Override
+    public void onMessage(String callIdentifier, String message) {
+
+    }
+
+    @Override
+    public void onMessage(String callIdentifier, Integer message) {
+
+    }
+
+    @Override
+    public void onMessage(String callIdentifier, JSONObject message) {
+
+    }
+
+    void onSpeakerChange(SCClientEvent parameter) {
+        sendEvent("onSpeakerChange", parameter);
+    }
+
+    void onMuteChange(SCClientEvent parameter) {
+        sendEvent("onMuteChange", parameter);
+    }
+
+
+
+    void onUserInterfaceRequest(SCClientEvent parameter) {
+
+        sendEvent("onUserInterfaceRequest", parameter);
+    }
+
+    /**
+     *  convert a SCClient Event Instance to a JSONObject in string format
+     *  in order to transmit it to the RN
+     *
+     * @param event an Instance of SCClientEvent that represent the state of the device at this
+     *  moment. Also host the state of the call and transform it.
+     *
+     * @return the JsonObject reprensenting the state in String format
+     */
+    String convertSCClientEventToJSONString(SCClientEvent event) {
+
+        try {
+            if (event != null) {
+                JSONObject jsonEvent = convertSCClientEventToJSON(event);
+                return jsonEvent.toString();
+            }
+        } catch (JSONException e){
+            Log.e(TAG, "convertSCClientEventToJSONString: failed to convert to json", e);
+        }
+        return null;
+    }
+
+    /**
+     *  convert a SCClient Event Instance to a JSONObject in order to transmit it to the RN
+     *
+     * @param event an Instance of SCClientEvent that represent the state of the device at this
+     * moment. Also host the state of the call and transform it.
+     *
+     * @return the JsonObject reprensenting the state
+     * @throws JSONException if an error occur in the transformation.
+     */
+    private JSONObject convertSCClientEventToJSON(SCClientEvent event) throws JSONException {
+
+        JSONObject jsonEvent = new JSONObject();
+        jsonEvent.accumulate("connected", event.isConnected());
+        jsonEvent.accumulate("mute", event.isMute());
+        jsonEvent.accumulate("speaker", event.isSpeaker());
+        jsonEvent.accumulate("call", convertSCCallToJson(event.getCall()));
+
+        return jsonEvent;
+    }
+
+    /**
+     *  convert a SCCall instance to a JSON Object in order to send it to the RN in Front
+     *
+     * @param call a SCCall instance that represent the state of the call at current moment
+     * @return the SCCall as a JSONObject Instance.
+     * @throws JSONException if a
+     */
+    private JSONObject convertSCCallToJson(SCCall call) throws JSONException {
+
+        JSONObject jsonCall = new JSONObject();
+
+        if (call == null)
+            return null;
+        jsonCall.accumulate("callIdentifier", call.getCallIdentifier());
+        jsonCall.accumulate("agentMail", call.getAgentMail());
+        jsonCall.accumulate("callState", call.getCurrentCallState());
+        jsonCall.accumulate("date", call.getDate());
+        jsonCall.accumulate("brand", call.getDisplayBrand());
+        jsonCall.accumulate("name", call.getDisplayName());
+        jsonCall.accumulate("duration", call.getDuration());
+        jsonCall.accumulate("startedTime", call.getStartedTime());
+
+        return jsonCall;
+    }
+}
