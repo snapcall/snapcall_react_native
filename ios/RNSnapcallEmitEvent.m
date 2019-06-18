98d121ca3316978f06a5f9f717036057eebf691e
pnoyelle <noyelle.pierre@gmail.com>
2019-06-18 19:10:56 +0200

    IOS Added  : event Emiter class
    This singleton class is deisgned to send the event to the JS.
    The event list has been changed to follow the new SCClient system :
    "onConnectionReady", "onCreated", "onUIRequest", "onRinging" , "onAnswer", "onInternetDown", "onInternetUP", "onHangup",
                 "onHeld", "onUnheld", "onMuteChange", "onSpeakerChange", "onTime", "onConnectionShutDown"

---
+++ ios/RNSnapcallEmitEvent.m
@@ -0,0 +1,55 @@
+//
+//  RNSnapcallEmitEvent.m
+//  RNSnapcallReact
+//
+//  Created by Noyelle on 13/06/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RNSnapcallEmitEvent.h"
+
+@implementation RNSnapcallEmitEvent
+
+RCT_EXPORT_MODULE(RNSnapcallEmitEvent);
+
+static RNSnapcallEmitEvent *instance = nil;
+
++(id)allocWithZone:(NSZone *)zone {
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        
+        instance = [super allocWithZone:zone];
+        
+    });
+    return instance;
+}
+
++ (RNSnapcallEmitEvent *)getInstance {
+    return instance;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
+- (NSArray<NSString *> *)supportedEvents{
+    return @[@"onConnectionReady", @"onCreated", @"onUIRequest", @"onRinging" , @"onAnswer", @"onInternetDown", @"onInternetUP", @"onHangup",
+             @"onHeld", @"onUnheld", @"onMuteChange", @"onSpeakerChange", @"onTime", @"onConnectionShutDown"];
+}
+
+- (id) preventNilForValue: (id) value {
+    
+    if (value != nil) {
+        return value;
+    }
+    return [NSNull null];
+}
+
+- (void) sendEventWithName:(NSString *)name parameter:(NSDictionary * _Nonnull)parameter {
+    [self sendEventWithName:name body:parameter];
+}
+
+@end
