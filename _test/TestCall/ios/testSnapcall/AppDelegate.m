e36214a3f0ed2cc171e9f73045d68e25e71cd46a
pnoyelle <noyelle.pierre@gmail.com>
2021-08-16 18:05:01 +0200

    update test app - video and push norification on ios

--- _test/TestCall/ios/testSnapcall/AppDelegate.m
+++ _test/TestCall/ios/testSnapcall/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSnapcallEventListener.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -35,13 +36,13 @@ static void InitializeFlipper(UIApplication *application) {
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"TestCall2"
                                             initialProperties:nil];
-
+  
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
       rootView.backgroundColor = [UIColor whiteColor];
   }
-
+  [self voipRegistration];
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
@@ -57,6 +58,51 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (void) voipRegistration {
+  printf("register for voip push\n");
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    // Create a push registry object
+    PKPushRegistry * voipRegistry = [[PKPushRegistry alloc] initWithQueue: mainQueue];
+    // Set the registry's delegate to self
+    voipRegistry.delegate = self;
+    // Set the push type to VoIP
+    voipRegistry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
+    if ([voipRegistry pushTokenForType: PKPushTypeVoIP] == nil) {
+      NSLog(@"credential is null");
+    } else {
+      NSLog([voipRegistry pushTokenForType: PKPushTypeVoIP]);
+    }
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type {
+    if([credentials.token length] == 0) {
+        NSLog(@"voip token NULL");
+        return;
+    }
+    
+    NSLog(@"PushCredentials: %@", credentials.token);
+    const char *tokenBytes = (const char *)[credentials.token bytes];
+    NSMutableString *deviceTokenString = [NSMutableString string];
+    for (NSUInteger i = 0; i < [credentials.token length]; i++) {
+      [deviceTokenString appendFormat:@"%02.2hhx", tokenBytes[i]];
+    }
+    NSLog(@"token");
+    NSLog(deviceTokenString);
+    [RNSnapcallReact reportVoipToken: deviceTokenString];
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(NSString *)type {
+    // Process the received push
+    NSLog(@"Get");
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion {
+    //available(iOS 11.0, *)
+    NSLog(@"Get");
+    SnapcallExternalParameter *param = [[SnapcallExternalParameter alloc]  init];
+    [RNSnapcallReact receiveCallWith: payload parameter: param];
 }
 
 @end
