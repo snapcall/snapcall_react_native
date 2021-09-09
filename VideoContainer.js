01f19865ce159e6c946cb0a47a9d69eee25bad71
pnoyelle <noyelle.pierre@gmail.com>
2021-09-09 17:58:19 +0200

    add video display type

--- VideoContainer.js
+++ VideoContainer.js
@@ -5,6 +5,7 @@ import { requireNativeComponent, View } from 'react-native';
 var iface = {
     name: 'VideoContainer',
     PropTypes: {
+        displayType: PropTypes.string,
         videosrc: PropTypes.string,
         ...View.propTypes // include the default view properties
     }
