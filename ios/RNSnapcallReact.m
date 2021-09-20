a3972988b0d84ebf6e655a4c61578b22e69d1fb9
pnoyelle <noyelle.pierre@gmail.com>
2021-09-20 16:58:49 +0200

    update ios sdk to ios 5.9

--- ios/RNSnapcallReact.m
+++ ios/RNSnapcallReact.m
@@ -510,8 +510,8 @@ RCT_REMAP_METHOD(disconnect,  Promise:(RCTPromiseResolveBlock)resolve rejecter:(
     }
 }
 
-RCT_REMAP_METHOD(sendPartnerCallInvitationWithToken, partnerID:(int)partnerID token: (NSString*)token chatID: (NSString*)chatID param: (NSString*)param  Promise:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
-    [[Snapcall getSnapcall] sendPartnerCallInvitationWithPartnerID:partnerID token:token chatID:chatID :^(NSError * error) {
+RCT_REMAP_METHOD(sendPartnerCallInvitationWithToken, partnerID:(int)partnerID token: (NSString*)token chatID: (NSString*)chatID param: (NSDictionary*)param customParameter: (NSDictionary*) customParameter Promise:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
+    [[Snapcall getSnapcall] sendPartnerCallInvitationWithPartnerID:partnerID token:token chatID:chatID customParameter: customParameter :^(NSError * error) {
         if (error != nil) {
             reject(@"-1", @"sendPartnerCall error", error);
             return;
@@ -520,10 +520,10 @@ RCT_REMAP_METHOD(sendPartnerCallInvitationWithToken, partnerID:(int)partnerID to
     }];
 }
 
-RCT_REMAP_METHOD(connectSendPartnerCallInvitationWithToken, partnerID:(int)partnerID agent: (NSString*)agent token: (NSString*)token chatID: (NSString*)chatID param: (NSDictionary*)param  Promise:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
+RCT_REMAP_METHOD(connectSendPartnerCallInvitationWithToken, partnerID:(int)partnerID agent: (NSString*)agent token: (NSString*)token chatID: (NSString*)chatID param: (NSDictionary*)param customParameter: (NSDictionary*) customParameter  Promise:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     printf("testing connect partner\n");
     
-    [[Snapcall getSnapcall] sendPartnerCallInvitationWithPartnerID:partnerID agent:agent token:token chatID: chatID parameter:[self snapcallParamFromDictionnary:param] : ^(NSError * error, Agent* agent) {
+    [[Snapcall getSnapcall] sendPartnerCallInvitationWithPartnerID:partnerID agent:agent token:token chatID: chatID customParameter: customParameter parameter:[self snapcallParamFromDictionnary:param] : ^(NSError * error, Agent* agent) {
         if (error != nil) {
             NSLog(@"on error %s", error.description);
             reject(@"-1", @"sendPartnerCall error", error);
